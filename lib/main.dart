d7219f7e7fee914acb73fc176873e4f5f110446a
Sandeep <sandeeepkumaar.0034@gmail.com>
2021-02-27 16:14:10 +0530

    Adding the images

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,7 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
-          ),
+          child: Image(image: AssetImage('images/diamond.png')),
         ),
       ),
     ),
