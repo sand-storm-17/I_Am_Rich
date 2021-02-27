2a10f21d486c2535068385afbfdc84a9b9587057
Sandeep <sandeeepkumaar.0034@gmail.com>
2021-02-27 16:01:03 +0530

    Adding project files for i_am_rich app

---
+++ android/app/src/main/kotlin/com/ksandeep17/i_am_riches/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ksandeep17.i_am_riches
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[100],
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
